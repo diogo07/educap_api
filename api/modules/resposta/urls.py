0204c9a188c372011c4d7cfc31e692125b781ab7
diogo07 <diogosousa36@gmail.com>
2020-10-04 09:18:45 -0300

    adicionando query de filtro por questão

--- api/modules/resposta/urls.py
+++ api/modules/resposta/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("<int:pk>", RespostaGetView.as_view(), name="resposta_get"),
     path("universidade/<int:id_universidade>/ano/<int:ano>", RespostaFilterByUniversidadeEAno.as_view(), name="resposta_by_universidade"),
     path("universidade/<int:id_universidade>/curso/<int:codigo_grupo>/ano/<int:ano>", RespostaFilterByUniversidadeCursoEAno.as_view(), name="resposta_by_universidade_and_curso"),
+    path("universidade/<int:id_universidade>/ano/<int:ano>/questao/<str:questao>", RespostaFilterByUniversidadeAnoEQuestao.as_view(), name="resposta_by_universidade_ano_e_questao"),
+    path("universidade/<int:id_universidade>/curso/<int:codigo_grupo>/ano/<int:ano>/questao/<str:questao>", RespostaFilterByUniversidadeCursoAnoEQuestao.as_view(), name="resposta_by_universidade_curso_ano_e_questao"),
 
 ]
--- api/modules/resposta/views.py
+++ api/modules/resposta/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
-from django.http import JsonResponse
+from django.db import connection
+
 
 from api.modules.resposta.serializers import *
 from api.modules.basicApiView import *
@@ -44,4 +45,55 @@ class RespostaFilterByUniversidadeCursoEAno(IsAutenticatedListApiView):
         queryset = Resposta.objects.filter(id_aluno__id_curso__id_universidade=id, id_aluno__id_curso__codigo_grupo=codigo_grupo, id_aluno__enade_ano=ano).values('codigo_questao', 'opcao').annotate(
                 total=Count('opcao'))
 
+        return queryset
+
+class RespostaFilterByUniversidadeAnoEQuestao(IsAutenticatedListApiView):
+    serializer_class = RespostaFilterByUniversidadeSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id_universidade']
+        ano = self.kwargs['ano']
+        codigo_questao = self.kwargs['questao']
+        cursor = connection.cursor()
+        cursor.execute("select r.codigo_questao as codigo_questao, r.opcao as opcao, count(r.opcao) as total from resposta as r"+
+                                        " inner join aluno as al on al.id = r.id_aluno"+
+                                        " inner join curso as cr on cr.id = al.id_curso"+
+                                        " inner join universidade as u on u.id = cr.id_universidade"+
+                                        " inner join questao as q on q.codigo = r.codigo_questao"+
+                                        " where r.codigo_questao = '"+codigo_questao+"' and u.id = "+str(id)+" and al.enade_ano = "+str(ano)+""+
+                                        " group by (r.opcao, r.codigo_questao)")
+        queryset = []
+        for q in cursor.fetchall():
+            queryset.append({
+                'codigo_questao': q[0],
+                'opcao': q[1],
+                'total': q[2]
+            })
+        return queryset
+
+class RespostaFilterByUniversidadeCursoAnoEQuestao(IsAutenticatedListApiView):
+    serializer_class = RespostaFilterByUniversidadeSerializer
+
+    def get_queryset(self):
+        id = self.kwargs['id_universidade']
+        ano = self.kwargs['ano']
+        codigo_grupo = self.kwargs['codigo_grupo']
+        codigo_questao = self.kwargs['questao']
+        cursor = connection.cursor()
+        cursor.execute(
+            "select r.codigo_questao as codigo_questao, r.opcao as opcao, count(r.opcao) as total from resposta as r" +
+            " inner join aluno as al on al.id = r.id_aluno" +
+            " inner join curso as cr on cr.id = al.id_curso" +
+            " inner join universidade as u on u.id = cr.id_universidade" +
+            " inner join questao as q on q.codigo = r.codigo_questao" +
+            " where r.codigo_questao = '" + codigo_questao + "' and u.id = " + str(id) + " and al.enade_ano = " + str(
+                ano) + " and cr.codigo_grupo = " + str(codigo_grupo) +
+            " group by (r.opcao, r.codigo_questao)")
+        queryset = []
+        for q in cursor.fetchall():
+            queryset.append({
+                'codigo_questao': q[0],
+                'opcao': q[1],
+                'total': q[2]
+            })
         return queryset
